9b13ad697cc117ee5116fde1808742a3039739b6
AdrianKnudsen <adrian@knudsen.no>
2024-03-14 12:33:54 +0100

    added img gallery
    Added image gallery with a spin function when screen gets smaller or bigger.

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// Hero parallax effect
+
 document.addEventListener("scroll", () => {
   const scrollPosition = window.scrollY;
   const heroText = document.querySelector(".heroText");
@@ -7,3 +9,24 @@ document.addEventListener("scroll", () => {
 
   heroText.style.transform = `scale(${scale}) translateY(-${translateY}px)`;
 });
+
+// Gallery spin function
+
+function updateRotation() {
+  const screenWidth = window.innerWidth;
+  const maxWidth = 800;
+  let rotationDegree = Math.max(
+    0,
+    Math.min(1, (screenWidth - maxWidth) / maxWidth)
+  );
+
+  const rotationX = 45 * rotationDegree;
+  const rotationZ = 45 * rotationDegree;
+
+  const imageGrid = document.querySelector(".image-grid");
+  imageGrid.style.transform = `rotateX(${rotationX}deg) rotateZ(${rotationZ}deg)`;
+}
+
+window.addEventListener("resize", updateRotation);
+
+updateRotation();
