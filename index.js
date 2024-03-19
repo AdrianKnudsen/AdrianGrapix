547896fd0cf84e779364e2e2d7f3ca08da7d948a
AdrianKnudsen <adrian@knudsen.no>
2024-03-19 11:11:34 +0100

    added gallery function
    Now when you click a image in the gallery the image pops up and displays on the screen.

--- index.js
+++ index.js
@@ -30,3 +30,39 @@ function updateRotation() {
 window.addEventListener("resize", updateRotation);
 
 updateRotation();
+
+// Gallery image show "function"
+
+document.querySelectorAll(".tile-img").forEach((item) => {
+  item.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const overlay = document.getElementById("overlay");
+    overlay.style.display = "flex";
+
+    // Creates a new img element
+    const enlargedImg = document.createElement("img");
+    enlargedImg.setAttribute("src", this.src);
+    enlargedImg.classList.add("enlarged-img");
+    enlargedImg.style.transform = "scale(0.1)";
+
+    overlay.innerHTML = "";
+    overlay.appendChild(enlargedImg);
+
+    setTimeout(() => {
+      enlargedImg.style.transform = "scale(1.05)";
+    }, 10);
+  });
+});
+
+document.getElementById("overlay").addEventListener("click", function (e) {
+  e.stopPropagation();
+  const enlargedImg = this.querySelector(".enlarged-img");
+  if (enlargedImg) {
+    enlargedImg.style.transform = "scale(0.5)";
+    setTimeout(() => {
+      this.style.display = "none";
+      enlargedImg.style.transform = "scale(1)";
+    }, 500);
+  }
+});
